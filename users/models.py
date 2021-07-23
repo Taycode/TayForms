95a6a314e81d6e03ba0c8695f7076129a4592a73
Abdulmateen Tairu <tay2druh@gmail.com>
2021-07-23 22:47:33 +0100

    Custom User

--- users/models.py
+++ users/models.py
@@ -1,3 +1,17 @@
-from django.db import models
+"""User Model"""
 
-# Create your models here.
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.utils.translation import ugettext_lazy as _
+
+
+class User(AbstractUser):
+	"""Custom User Model"""
+	username = None
+	email = models.EmailField(_('email address'), unique=True)
+
+	USERNAME_FIELD = 'email'
+	REQUIRED_FIELDS = []
+
+	def __str__(self):
+		return self.email
